48d7c72927219b3c889665cbc7aaf65a7fe6e2ea
Mia <miamercandalli@gmail.com>
2025-05-05 18:23:53 +0100

    add WHERE, LIKE and IN

---
+++ between.sql
@@ -0,0 +1,12 @@
+--SELECT *
+--FROM planets
+--WHERE Distance_from_sun_km BETWEEN 90000000 AND 1000000000;
+
+--SELECT *
+--FROM astronauts
+--WHERE Name LIKE 'Neil%'
+
+
+SELECT *
+FROM astronauts
+WHERE Missions_count BETWEEN 2 AND 5;
--- where.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM astronauts
-WHERE [Status] = 'Retired' AND Total_time_in_space_hours > 255
-
